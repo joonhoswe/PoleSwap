6b0a417f2663631b2c77a7c921a5b74749279529
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2024-12-27 21:41:36 -0500

    mobile friendly tweaks

--- src/general/Layout.jsx
+++ src/general/Layout.jsx
@@ -2,7 +2,7 @@ import { Navbar } from '../components/Navbar'
 
 export const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen w-full px-12 items-center text-black">
+    <div className="flex flex-col min-h-screen w-full px-4 lg:px-12 items-center text-black">
       <Navbar/>
       {children}
     </div>
