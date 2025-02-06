c81e54c528c77171502cc412acfde1139792f0ab
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2025-02-06 09:56:56 -0500

    home and footer padding changes

--- src/general/Layout.jsx
+++ src/general/Layout.jsx
@@ -3,9 +3,9 @@ import { Footer } from '../components/Footer';
 
 export const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen w-full px-4 lg:px-12 items-center text-black">
+    <div className="flex flex-col min-h-screen h-auto w-full px-4 lg:px-12 items-center text-black">
       <Navbar/>
-      <main className="flex-1 min-w-full flex flex-col">
+      <main className="flex-1 min-w-full min-h-screen flex flex-col">
         {children}
       </main>
       <Footer />
